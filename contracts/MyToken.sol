8e380e0e05271b2614c82d37948a9cb648161644
dedane <evanskeema@gmail.com>
2021-10-27 22:35:09 +0300

    crowdsale

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Supermarket Loyalty Token", "SLT") public {
+    constructor(uint256 initialSupply) ERC20("Supermarket Loyalty Token", "SLT")  {
         _mint(msg.sender, initialSupply);
     
     }
--- test/MyToken.Test.js
+++ test/MyToken.Test.js
@@ -13,10 +13,31 @@ chai.use(chaiAsPromised)
 const expect = chai.expect;
 
 contracts_build_directory("Token Test",async (accounts) => {
+
+    const [deployerAccount, recipient, anotherAccount] = accounts;
+
     it("All Tokens should be in my account", async() => {
         let instance = await Token.deployed()
         let TotalSupply = await istance.totalSupply()
+        //let balance = await instance of balanceOf(account[0])
+        //assert.equal(balance.valueOf(), initialSupplly.valueOf(), "The balance was not the same")
         expect( await instance.balanceOf(accounts[0])).to.be.a.bignumber.equal(totalSupply);        
+    })
 
+    it("is possible to send tokens between accounts", async()=> {
+        const sendTokens = 1;
+        let instance = await Token.deployed();
+        let totalsupply = await instance.totalsupply();
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bigNumber.equal(totalsupply);
+        expect(instance.balanceOf(recipient, sendTokens)).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bigNumber.equal(totalsupply).sub(new BN(sendTokens));
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bigNumber.equal(new BN(sendTokens))
+    })
+
+    it("is not possible to send more tokens than available in total", async()=> {
+        let instance = await Token.deployed()
+        let balanceOfDeployer = await instance.balanceOf(deployerAccount)
+        expect(instance.transfer(recipient, new BN(balanceOfDeployer+1))).to.eventually.be.fulfilled
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bigNumber.equal(balanceOfDeployer)
     })
 })
